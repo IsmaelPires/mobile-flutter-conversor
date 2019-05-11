4eb301e599a4f73790e221d4c4105874d60972c4
Ismael Fernandes <ismaa_gyn@hotmail.com>
2019-05-11 11:32:19 -0300

    Criando coluna e mudando cor do background

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,16 @@ class _HomeState extends State<Home> {
       ],
     );
 
+    Column column = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[],
+
+    );
+
     Scaffold scaffold = Scaffold(
       appBar: appBar,
+      backgroundColor: Colors.grey,
+      body: column,
     );
 
     return scaffold;
